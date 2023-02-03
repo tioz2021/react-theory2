9ab3403ced0c66d5e62d0e7fecbf57ed70c943a6
tios <jenya25life@gmail.com>
2023-02-03 20:44:29 +0200

    load project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import React from 'react'
+import Character from './Character'
 
-function App() {
+const App = ({side}) => {
+  if (!side) {
+    side = 'light'
+  }
+
+  const characters = [
+    {name: 'Люк Скайвокер', side: 'light'},
+    {name: 'Йода', side: 'light'},
+    {name: 'Обиван Кеноби', side: 'light'},
+    {name: 'Палпатин', side: 'dark'},
+    {name: 'Дарт Вэйдер', side: 'dark'}
+  ]
+
+  const filteredChars = characters.filter(char => char.side === side)
+
   return (
-    <div>hello</div>
-  );
+    <ul>
+      {filteredChars.map((char, index) => (
+        <Character key={char.name + index} name={char.name} side={char.side} />
+      ))}
+    </ul>
+  )
 }
 
-export default App;
+export default App
